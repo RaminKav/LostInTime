4ac4c734bfe4c0341c75e4bbf434869932b01008
Ramin <raminkaviani@live.ca>
2024-07-06 01:36:27 -0400

    fix: small era bug fixes

--- src/vectorize.rs
+++ src/vectorize.rs
@@ -24,3 +24,38 @@ pub mod vectorize {
         Ok(T::from_iter(container.into_iter()))
     }
 }
+pub mod vectorize_inner {
+    use bevy::utils::HashMap;
+    use serde::{Deserialize, Deserializer, Serialize, Serializer};
+    use std::iter::FromIterator;
+
+    pub fn serialize<K, V, S>(target: &Vec<HashMap<K, V>>, ser: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+        K: Serialize,
+        V: Serialize,
+    {
+        let serialized: Vec<_> = target
+            .into_iter()
+            .map(|hashmap| hashmap.into_iter().collect::<Vec<_>>())
+            .collect();
+
+        serde::Serialize::serialize(&serialized, ser)
+    }
+
+    pub fn deserialize<'de, K, V, D>(des: D) -> Result<Vec<HashMap<K, V>>, D::Error>
+    where
+        D: Deserializer<'de>,
+        K: Deserialize<'de> + Eq + std::hash::Hash,
+        V: Deserialize<'de>,
+    {
+        let deserialized: Vec<Vec<(K, V)>> = serde::Deserialize::deserialize(des)?;
+
+        let result: Vec<HashMap<K, V>> = deserialized
+            .into_iter()
+            .map(|vec| HashMap::from_iter(vec))
+            .collect();
+
+        Ok(result)
+    }
+}
