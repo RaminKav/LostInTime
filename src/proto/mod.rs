a61da40b0989252b66f1eb1e1c2484333bbae59b
Ari Lotter <arilotter@gmail.com>
2024-08-22 17:36:31 -0400

    refactor proto loading
    just load everything from the folder!

--- src/proto/mod.rs
+++ src/proto/mod.rs
@@ -8,8 +8,9 @@ use bevy::{
     utils::HashMap,
 };
 use bevy_proto::{
-    backend::schematics::FromSchematicInput,
+    backend::{proto, schematics::FromSchematicInput},
     prelude::{Prototypes, PrototypesMut, ReflectSchematic, Schematic, SchematicContext},
+    proto::Prototype,
 };
 use bevy_rapier2d::prelude::{Collider, KinematicCharacterController, QueryFilterFlags, Sensor};
 use strum::IntoEnumIterator;
@@ -145,249 +146,33 @@ impl Plugin for ProtoPlugin {
             .register_type_data::<RangeInclusive<i32>, ReflectDeserialize>()
             .add_plugin(bevy_proto::prelude::ProtoPlugin::new())
             .add_system(apply_system_buffers.in_set(CustomFlush))
-            .add_system(Self::load_prototypes.in_set(OnUpdate(GameState::Loading)))
-            .add_system(
-                Self::check_proto_ready
-                    .after(Self::load_prototypes)
-                    .in_set(OnUpdate(GameState::Loading)),
-            );
+            .add_system(Self::load_prototypes.in_schedule(OnEnter(GameState::Loading)))
+            .add_system(Self::check_proto_ready.in_set(OnUpdate(GameState::LoadingProtos)));
     }
 }
 
+#[derive(Resource)]
+struct AllProtos(Vec<HandleUntyped>);
+
 impl ProtoPlugin {
-    fn load_prototypes(mut prototypes: PrototypesMut) {
+    fn load_prototypes(mut prototypes: PrototypesMut, mut commands: Commands) {
         info!("Loading prototypes...");
-        //TODO: automate this
-        prototypes.load("proto/item_drop.prototype.ron");
-        prototypes.load("proto/world_object.prototype.ron");
-        prototypes.load("proto/dodge.prototype.ron");
-        prototypes.load("proto/tooltipinspect.prototype.ron");
-        prototypes.load("proto/smallgreentree.prototype.ron");
-        prototypes.load("proto/inventorybag.prototype.ron");
-        prototypes.load("proto/redtree.prototype.ron");
-        prototypes.load("proto/smallyellowtree.prototype.ron");
-        prototypes.load("proto/smallyellowtree.prototype.ron");
-        prototypes.load("proto/mediumyellowtree.prototype.ron");
-        prototypes.load("proto/mediumgreentree.prototype.ron");
-        prototypes.load("proto/Era1WorldGenerationParams.prototype.ron");
-        prototypes.load("proto/DungeonWorldGenerationParams.prototype.ron");
-        prototypes.load("proto/stonewall.prototype.ron");
-        prototypes.load("proto/stonewallblock.prototype.ron");
-        prototypes.load("proto/projectile.prototype.ron");
-        prototypes.load("proto/rock.prototype.ron");
-        prototypes.load("proto/fireball.prototype.ron");
-        prototypes.load("proto/electricity.prototype.ron");
-        prototypes.load("proto/arc.prototype.ron");
-        prototypes.load("proto/fireattack.prototype.ron");
-        prototypes.load("proto/redmushking.prototype.ron");
-        prototypes.load("proto/sword.prototype.ron");
-        prototypes.load("proto/chestplate.prototype.ron");
-        prototypes.load("proto/metalpants.prototype.ron");
-        prototypes.load("proto/metalshoes.prototype.ron");
-        prototypes.load("proto/leatherpants.prototype.ron");
-        prototypes.load("proto/forestpants.prototype.ron");
-        prototypes.load("proto/forestshirt.prototype.ron");
-        prototypes.load("proto/forestshoes.prototype.ron");
-        prototypes.load("proto/leathertunic.prototype.ron");
-        prototypes.load("proto/leathershoes.prototype.ron");
-        prototypes.load("proto/dagger.prototype.ron");
-        prototypes.load("proto/basicstaff.prototype.ron");
-        prototypes.load("proto/dualstaff.prototype.ron");
-        prototypes.load("proto/firestaff.prototype.ron");
-        prototypes.load("proto/ring.prototype.ron");
-        prototypes.load("proto/pendant.prototype.ron");
-        prototypes.load("proto/flint.prototype.ron");
-        prototypes.load("proto/smallpotion.prototype.ron");
-        prototypes.load("proto/largepotion.prototype.ron");
-        prototypes.load("proto/log.prototype.ron");
-        prototypes.load("proto/mob_basic.prototype.ron");
-        prototypes.load("proto/slime.prototype.ron");
-        prototypes.load("proto/spikeslime.prototype.ron");
-        prototypes.load("proto/furdevil.prototype.ron");
-        prototypes.load("proto/chest.prototype.ron");
-        prototypes.load("proto/pebble.prototype.ron");
-        prototypes.load("proto/chestblock.prototype.ron");
-        prototypes.load("proto/dungeonentrance.prototype.ron");
-        prototypes.load("proto/dungeonentranceblock.prototype.ron");
-        prototypes.load("proto/grass.prototype.ron");
-        prototypes.load("proto/grass2.prototype.ron");
-        prototypes.load("proto/grass3.prototype.ron");
-        prototypes.load("proto/grassblock.prototype.ron");
-        prototypes.load("proto/boulder.prototype.ron");
-        prototypes.load("proto/stick.prototype.ron");
-        prototypes.load("proto/woodplank.prototype.ron");
-        prototypes.load("proto/woodaxe.prototype.ron");
-        prototypes.load("proto/plantfibre.prototype.ron");
-        prototypes.load("proto/slimegoo.prototype.ron");
-        prototypes.load("proto/bandage.prototype.ron");
-        prototypes.load("proto/string.prototype.ron");
-        prototypes.load("proto/deadsapling.prototype.ron");
-        prototypes.load("proto/apple.prototype.ron");
-        prototypes.load("proto/magicwhip.prototype.ron");
-        prototypes.load("proto/woodbow.prototype.ron");
-        prototypes.load("proto/arrow.prototype.ron");
-        prototypes.load("proto/greenwhip.prototype.ron");
-        prototypes.load("proto/claw.prototype.ron");
-        prototypes.load("proto/throwingstar.prototype.ron");
-        prototypes.load("proto/fireexplosionaoe.prototype.ron");
-        prototypes.load("proto/crate.prototype.ron");
-        prototypes.load("proto/crateblock.prototype.ron");
-        prototypes.load("proto/coal.prototype.ron");
-        prototypes.load("proto/metalshard.prototype.ron");
-        prototypes.load("proto/metalbar.prototype.ron");
-        prototypes.load("proto/coalboulder.prototype.ron");
-        prototypes.load("proto/metalboulder.prototype.ron");
-        prototypes.load("proto/slimegooprojectile.prototype.ron");
-        prototypes.load("proto/stonechunk.prototype.ron");
-        prototypes.load("proto/woodsword.prototype.ron");
-        prototypes.load("proto/pebbleblock.prototype.ron");
-        prototypes.load("proto/redmushroom.prototype.ron");
-        prototypes.load("proto/redmushroomblock.prototype.ron");
-        prototypes.load("proto/brownmushroom.prototype.ron");
-        prototypes.load("proto/brownmushroomblock.prototype.ron");
-        prototypes.load("proto/berrybush.prototype.ron");
-        prototypes.load("proto/berries.prototype.ron");
-        prototypes.load("proto/feather.prototype.ron");
-        prototypes.load("proto/woodpickaxe.prototype.ron");
-        prototypes.load("proto/hog.prototype.ron");
-        prototypes.load("proto/mob_passive.prototype.ron");
-        prototypes.load("proto/tusk.prototype.ron");
-        prototypes.load("proto/bed.prototype.ron");
-        prototypes.load("proto/bedblock.prototype.ron");
-        prototypes.load("proto/magictusk.prototype.ron");
-        prototypes.load("proto/magicgem.prototype.ron");
-        prototypes.load("proto/leather.prototype.ron");
-        prototypes.load("proto/rawmeat.prototype.ron");
-        prototypes.load("proto/cookedmeat.prototype.ron");
-        prototypes.load("proto/dirtpath.prototype.ron");
-        prototypes.load("proto/stingfly.prototype.ron");
-        prototypes.load("proto/bushling.prototype.ron");
-        prototypes.load("proto/redmushling.prototype.ron");
-        prototypes.load("proto/crate2.prototype.ron");
-        prototypes.load("proto/bushlingscale.prototype.ron");
-        prototypes.load("proto/bush.prototype.ron");
-        prototypes.load("proto/bush2.prototype.ron");
-        prototypes.load("proto/boulder2.prototype.ron");
-        prototypes.load("proto/largestump.prototype.ron");
-        prototypes.load("proto/largemushroomstump.prototype.ron");
-        prototypes.load("proto/yellowflower.prototype.ron");
-        prototypes.load("proto/yellowflowerblock.prototype.ron");
-        prototypes.load("proto/redflower.prototype.ron");
-        prototypes.load("proto/redflowerblock.prototype.ron");
-        prototypes.load("proto/pinkflower.prototype.ron");
-        prototypes.load("proto/pinkflowerblock.prototype.ron");
-        prototypes.load("proto/stump.prototype.ron");
-        prototypes.load("proto/stump2.prototype.ron");
-        prototypes.load("proto/cattail.prototype.ron");
-        prototypes.load("proto/lillypad.prototype.ron");
-        prototypes.load("proto/waterboulder.prototype.ron");
-        prototypes.load("proto/waterboulder2.prototype.ron");
-        prototypes.load("proto/craftingtable.prototype.ron");
-        prototypes.load("proto/alchemytable.prototype.ron");
-        prototypes.load("proto/anvil.prototype.ron");
-        prototypes.load("proto/cauldron.prototype.ron");
-        prototypes.load("proto/furnace.prototype.ron");
-        prototypes.load("proto/craftingtableblock.prototype.ron");
-        prototypes.load("proto/alchemytableblock.prototype.ron");
-        prototypes.load("proto/anvilblock.prototype.ron");
-        prototypes.load("proto/cauldronblock.prototype.ron");
-        prototypes.load("proto/furnaceblock.prototype.ron");
-        prototypes.load("proto/redstew.prototype.ron");
-        prototypes.load("proto/upgradestation.prototype.ron");
-        prototypes.load("proto/upgradestationblock.prototype.ron");
-        prototypes.load("proto/upgradetome.prototype.ron");
-        prototypes.load("proto/orboftransformation.prototype.ron");
-        prototypes.load("proto/bridge.prototype.ron");
-        prototypes.load("proto/bridgeblock.prototype.ron");
-        prototypes.load("proto/largemanapotion.prototype.ron");
-        prototypes.load("proto/smallmanapotion.prototype.ron");
-        prototypes.load("proto/dungeonexit.prototype.ron");
-        prototypes.load("proto/woodwall.prototype.ron");
-        prototypes.load("proto/woodwallblock.prototype.ron");
-        prototypes.load("proto/wooddoor.prototype.ron");
-        prototypes.load("proto/wooddooropen.prototype.ron");
-        prototypes.load("proto/wooddoorblock.prototype.ron");
-        prototypes.load("proto/essence.prototype.ron");
-        prototypes.load("proto/key.prototype.ron");
-        prototypes.load("proto/fairy.prototype.ron");
-        prototypes.load("proto/miracleseed.prototype.ron");
-        prototypes.load("proto/combatshrine.prototype.ron");
-        prototypes.load("proto/combatshrinedone.prototype.ron");
-        prototypes.load("proto/gambleshrine.prototype.ron");
-        prototypes.load("proto/gambleshrinedone.prototype.ron");
-
-        // Sapplings
-        prototypes.load("proto/redsapplingblock.prototype.ron");
-        prototypes.load("proto/yellowsapplingblock.prototype.ron");
-        prototypes.load("proto/greensapplingblock.prototype.ron");
-        prototypes.load("proto/redsapplingstage1.prototype.ron");
-        prototypes.load("proto/greensapplingstage1.prototype.ron");
-        prototypes.load("proto/yellowsapplingstage1.prototype.ron");
-        prototypes.load("proto/redsapplingstage2.prototype.ron");
-        prototypes.load("proto/greensapplingstage2.prototype.ron");
-        prototypes.load("proto/yellowsapplingstage2.prototype.ron");
-        prototypes.load("proto/redsapplingstage3.prototype.ron");
-        prototypes.load("proto/greensapplingstage3.prototype.ron");
-        prototypes.load("proto/yellowsapplingstage3.prototype.ron");
-
-        // Era 2
-        prototypes.load("proto/era2smalltree.prototype.ron");
-        prototypes.load("proto/era2mediumtree.prototype.ron");
-        prototypes.load("proto/era2largetree.prototype.ron");
-        prototypes.load("proto/era2grass.prototype.ron");
-        prototypes.load("proto/era2grass2.prototype.ron");
-        prototypes.load("proto/era2grass3.prototype.ron");
-        prototypes.load("proto/era2stump.prototype.ron");
-        prototypes.load("proto/era2stump2.prototype.ron");
-        prototypes.load("proto/era2deadbranch.prototype.ron");
-        prototypes.load("proto/era2berrybush.prototype.ron");
-        prototypes.load("proto/era2boulder.prototype.ron");
-        prototypes.load("proto/era2boulder2.prototype.ron");
-        prototypes.load("proto/era2brownmushroom.prototype.ron");
-        prototypes.load("proto/era2brownmushroomblock.prototype.ron");
-        prototypes.load("proto/era2redmushroom.prototype.ron");
-        prototypes.load("proto/era2redmushroomblock.prototype.ron");
-        prototypes.load("proto/era2coalboulder.prototype.ron");
-        prototypes.load("proto/era2magicboulder.prototype.ron");
-        prototypes.load("proto/era2pebble.prototype.ron");
-        prototypes.load("proto/era2redflower.prototype.ron");
-        prototypes.load("proto/era2redflowerblock.prototype.ron");
-        prototypes.load("proto/era2whiteflower.prototype.ron");
-        prototypes.load("proto/era2whiteflowerblock.prototype.ron");
-        prototypes.load("proto/Era2WorldGenerationParams.prototype.ron");
-
-        prototypes.load("proto/bossshrine.prototype.ron");
-        prototypes.load("proto/timegate.prototype.ron");
-        prototypes.load("proto/timefragment.prototype.ron");
+        let handles = prototypes.load_folder("proto").unwrap();
+        commands.insert_resource(AllProtos(handles));
     }
-    fn check_proto_ready(prototypes: Prototypes) {
-        for obj in WorldObject::iter() {
-            if obj == WorldObject::None
-                || obj == WorldObject::WaterTile
-                || obj == WorldObject::GrassTile
-                || obj == WorldObject::StoneTile
-            {
-                continue;
-            }
-            let p = <WorldObject as Into<&str>>::into(obj.clone()).to_owned();
-
-            if !prototypes.is_ready(&p) {
-                warn!("proto {p:?} not ready");
+    fn check_proto_ready(
+        prototypes: Prototypes,
+        handles: Res<AllProtos>,
+        mut next_state: ResMut<NextState<GameState>>,
+    ) {
+        for p in &handles.0 {
+            if !prototypes.is_ready_handle(p) {
                 return;
             }
         }
-        for mob in Mob::iter() {
-            if mob == Mob::None {
-                continue;
-            }
-            let p = <Mob as Into<&str>>::into(mob.clone()).to_owned();
-
-            if !prototypes.is_ready(&p) {
-                warn!("proto {p:?} not ready");
-                return;
-            }
-        }
-        info!("READY, ENTERING GAME STATE");
+        let num = handles.0.len();
+        info!("All {num} prototypes ready. Moving to main menu!");
+        next_state.set(GameState::MainMenu);
     }
 }
 #[derive(Schematic, Reflect, FromReflect)]
