5c52d9f850400a575ad30d890f44988ed0e39f9e
Ari Lotter <arilotter@gmail.com>
2024-08-22 21:53:04 -0400

    move save data, logs, etc local in dev mode

--- src/datafiles.rs
+++ src/datafiles.rs
@@ -1,12 +1,22 @@
 use std::path::PathBuf;
 
+#[cfg(feature = "release-bundle")]
 use directories::ProjectDirs;
+#[cfg(feature = "release-bundle")]
 fn project_dir() -> ProjectDirs {
     ProjectDirs::from("com", "Ramin Kaviani", "Lost In Time").unwrap()
 }
 
+#[cfg(feature = "release-bundle")]
 fn game_dir() -> PathBuf {
     project_dir().data_dir().to_owned()
+}
+
+#[cfg(not(feature = "release-bundle"))]
+fn game_dir() -> PathBuf {
+    let mut b = PathBuf::new();
+    b.push("dev_data");
+    b
 }
 
 pub fn logs_dir() -> PathBuf {
