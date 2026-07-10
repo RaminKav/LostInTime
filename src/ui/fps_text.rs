5dbc960b496cba789f77030bcc3ce3d29b160be9
Ramin <raminkaviani@live.ca>
2026-07-09 22:27:48 -0400

    bpmp v 0.26.0

--- src/ui/fps_text.rs
+++ src/ui/fps_text.rs
@@ -8,7 +8,7 @@ use bevy::sprite::TextureAtlas;
 use bevy::text::TextLayoutInfo;
 
 use crate::{ui::game_fonts as gf, ScreenResolution, UICamera, DEBUG};
-const VERSION: &str = "v0.25.2";
+const VERSION: &str = "v0.26.0";
 #[derive(Component)]
 pub struct FPSText;
 
